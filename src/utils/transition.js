0afc501eb48a190f1faee0dfe9a172203bb89748
ahoopen <auke@frontmen.nl>
2015-07-03 22:26:50 +0200

    class for watching when a animation has ended

---
+++ src/utils/transition.js
@@ -0,0 +1,74 @@
+class transition {
+
+    /**
+     * Animation en transition end events.
+     *
+     * @type {string[]}
+     */
+    events = [
+        'transitionend',
+        'webkitTransitionEnd',
+        'oTransitionEnd',
+
+        'oAnimationEnd',
+        'webkitAnimationEnd',
+        'animationend'
+    ];
+
+    /**
+     * Puts a className on an element and listens when the animation
+     * is done.
+     *
+     * @param element
+     * @param className
+     * @returns {Promise}
+     */
+    animate(element, className) {
+
+        return new Promise((resolve, reject) => {
+            if (typeof element === 'undefined' || typeof className === 'undefined') {
+                reject();
+            }
+
+            this.element = element;
+            this.className = className;
+            this.resolve = resolve;
+
+            this.transitionEventListeners();
+
+            window.requestAnimationFrame(() => {
+                $(element).addClass(className);
+            });
+        });
+    }
+
+    /**
+     * Set all the animationEnd and transitionEnd event handlers
+     * on the element. if remove is true, delete the handlers
+     *
+     * @param remove
+     */
+    transitionEventListeners(remove) {
+        this.events.forEach((event) => {
+            if (!remove) {
+                $(this.element).one(event, this.resolvePromise.bind(this));
+            } else {
+                $(this.element).off(event);
+            }
+        });
+    }
+
+    /**
+     * clear event handlers, remove the class and then resolve
+     * the promise
+     *
+     * @param resolve
+     */
+    resolvePromise(resolve) {
+        this.transitionEventListeners(true);
+        $(this.element).removeClass(this.className);
+        this.resolve();
+    }
+}
+
+export default transition;
